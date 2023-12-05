af2fb9153c7498b8c4690ec1b5802feb5358565b
Sridhar Ratnakumar <srid@srid.ca>
2023-12-05 17:51:43 -0500

    nix: emanote, back to master

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   };
 
   inputs = {
-    emanote.url = "github:srid/emanote/callout"; # https://github.com/srid/emanote/pull/466
+    emanote.url = "github:srid/emanote";
     nixpkgs.follows = "emanote/nixpkgs";
     flake-parts.follows = "emanote/flake-parts";
   };
