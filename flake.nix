676191eb12c0285f4eb34903e833afdc626fff73
Sridhar Ratnakumar <srid@srid.ca>
2024-01-12 21:46:06 +0530

    Try a fork of emanote, to test a fix

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   };
 
   inputs = {
-    emanote.url = "github:srid/emanote";
+    emanote.url = "github:srid/emanote/stork-auto-layer";
     nixpkgs.follows = "emanote/nixpkgs";
     flake-parts.follows = "emanote/flake-parts";
   };
