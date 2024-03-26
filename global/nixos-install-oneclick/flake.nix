7a1301fc6dd91af97dcd7736a3b022b21aff1bdf
Sridhar Ratnakumar <srid@srid.ca>
2024-03-26 09:32:45 -0400

    key

--- global/nixos-install-oneclick/flake.nix
+++ global/nixos-install-oneclick/flake.nix
@@ -5,6 +5,8 @@
     disko.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, nixpkgs, disko, ... }: {
+    # A bare NixOS configuration that can be ssh'ed to as root, with known password
+    # For testing/demo purposes only.
     nixosConfigurations.oneclick = nixpkgs.lib.nixosSystem {
       # TODO: can we parametrize this? Maybe via --override-input?
       system = "aarch64-linux";
@@ -16,17 +18,14 @@
           boot.loader = {
             systemd-boot.enable = true;
             efi.canTouchEfiVariables = true;
-            grub = {
-              efiSupport = true;
-              # efiInstallAsRemovable = true;
-            };
           };
           networking.hostName = "oneclick";
           services.openssh.enable = true;
 
-          # Remove this after setting up users and keys
-          services.openssh.settings.PermitRootLogin = "yes";
-          users.users.root.initialHashedPassword = "root";
+          # Add your key here
+          users.users.root.openssh.authorizedKeys.keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQRxPoqlThDrkR58pKnJgmeWPY9/wleReRbZ2MOZRyd";
+          ];
 
           system.stateVersion = "23.11";
         })
