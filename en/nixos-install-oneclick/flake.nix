7ab500fe3a692ca749d4c47f8b2f7e175ae56a59
srid <srid@users.noreply.github.com>
2024-05-26 15:56:30 +0000

    deploy: de89dd986646acd8be8fb164e97328a18d42cc89

--- en/nixos-install-oneclick/flake.nix
+++ en/nixos-install-oneclick/flake.nix
@@ -4,29 +4,40 @@
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, disko, ... }: {
-    nixosConfigurations.oneclick = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, disko, ... }:
+    let
+      # TODO: Adjust these values to your needs
       system = "x86_64-linux";
-      modules = [
-        ./disk-config.nix
-        disko.nixosModules.disko
-        ({ pkgs, ... }: {
-          environment.systemPackages = [ pkgs.htop ];
-          boot.loader = {
-            systemd-boot.enable = true;
-            efi.canTouchEfiVariables = true;
-          };
-          networking.hostName = "oneclick";
-          services.openssh.enable = true;
-
-          # Add your key here
-          users.users.root.openssh.authorizedKeys.keys = [
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQRxPoqlThDrkR58pKnJgmeWPY9/wleReRbZ2MOZRyd"
-          ];
-
-          system.stateVersion = "23.11";
-        })
+      hostName = "oneclick";
+      rootAuthorizedKeys = [
+        # This user can ssh using `ssh root@<ip>`
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQRxPoqlThDrkR58pKnJgmeWPY9/wleReRbZ2MOZRyd"
       ];
+    in
+    {
+      nixosConfigurations.${hostName} = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./disk-config.nix
+          disko.nixosModules.disko
+          ({ pkgs, ... }: {
+            boot.loader = {
+              systemd-boot.enable = true;
+              efi.canTouchEfiVariables = true;
+            };
+            networking = { inherit hostName; };
+            services.openssh.enable = true;
+            environment.systemPackages = with pkgs; [
+              htop
+              git
+              neovim
+            ];
+
+            users.users.root.openssh.authorizedKeys.keys = rootAuthorizedKeys;
+
+            system.stateVersion = "23.11";
+          })
+        ];
+      };
     };
-  };
 }
